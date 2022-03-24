f7e09d8816a5afa2d16ce9b2c1a5b7c34a84c4cf
BryanVincent <102317980+BryanVincent@users.noreply.github.com>
2022-03-24 16:33:21 -0300

    Comentário que eu quiser

---
+++ teste2ifelaine.js
@@ -0,0 +1,32 @@
+console.log('Estações do ano')
+
+let mes = 'Dezembro'
+
+switch(mes){
+    case 'Janeiro':
+    case 'Fevereiro':
+    case 'Março':
+    console.log('Verão')
+    break
+
+    case 'Abril':
+    case 'Maio':
+    case 'Junho':
+    console.log('Outono')
+    break
+
+    case 'Julho':
+    case 'Agosto':
+    case 'Setembro':
+        console.log('Inverno')
+        break
+
+    case 'Outono':
+    case 'Novembro':
+    case 'Dezembro':
+        console.log('Primavera')
+        break
+
+
+
+}
---
+++ testeif.js
@@ -0,0 +1,20 @@
+let gasto = 1000
+
+if(gasto<2000){
+    console.log("OK! Gasto aprovado")
+}
+
+
+var nota1 = 8
+var nota2 = 4
+
+var media=(nota1 + nota2)/2
+
+if(media>=5){
+    console.log('Aprovada!')
+}
+else{console.log('Reprovada!')
+}
+
+
+
